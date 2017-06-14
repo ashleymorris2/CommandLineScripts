629d5f007fc3391e592c031da1e3972763435270
ashley.morris <ashley.morris@ultra-as.com>
2017-06-14 09:58:16 +0100

    Initial Commit 3

---
+++ format_milliseconds.py
@@ -0,0 +1,21 @@
+import click
+import moment
+from datetime import datetime
+
+# http://click.pocoo.org/5/quickstart/#quickstart
+
+@click.command()
+@click.argument('time')
+# @click.option('--time', prompt='The timestamp to format',
+# help='Formats a unix millisecond time stamp to human readable.')
+def format_millis(time):
+    """Formats a unix milliseconds timestamp to user readable string"""
+    try:
+        time = float(time)
+        formatted_time = moment.unix(time)
+        click.echo(formatted_time)
+    except ValueError:
+        click.echo("The value that was entered is not valid")
+
+if __name__ == '__main__':
+    format_millis()
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
    name='time_formatter',
-   version='0.1.0',
-   py_modules=['time_formatter'],
+   version='0.1.1',
+   py_modules=['format_millis'],
    description='Formats the millisecond time into human readable',
    author='Ashley Morris',
    author_email='foomail@foo.com',
    install_requires=['click', 'moment'], #external packages as dependencies
    entry_points='''
        [console_scripts]
-       format_millis=time_formatter:format_time
+       format_milliseconds=format_milliseconds:format_millis
    ''',
 )
