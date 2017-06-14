dc7c03f8e593bc5064671ab8fd1674e09ba81d6c
ashley.morris <ashley.morris@ultra-as.com>
2017-06-14 10:32:21 +0100

    difference between

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='time_formatter',
    version='0.1.1',
-   py_modules=['format_millis'],
+   py_modules=['format_milliseconds', 'time_between'],
    description='Formats the millisecond time into human readable',
    author='Ashley Morris',
    author_email='foomail@foo.com',
@@ -11,5 +11,6 @@ setup(
    entry_points='''
        [console_scripts]
        format_milliseconds=format_milliseconds:format_millis
+       time_between=time_between:between
    ''',
 )
---
+++ time_between.py
@@ -0,0 +1,51 @@
+import click
+import moment
+from datetime import datetime
+
+
+# http://click.pocoo.org/5/quickstart/#quickstart
+
+@click.command()
+@click.argument('time1')
+@click.argument('time2')
+# @click.option('--time', prompt='The timestamp to format',
+# help='Formats a unix millisecond time stamp to human readable.')
+def between(time1, time2):
+    """Calculate the difference between two unix timestamps"""
+    try:
+
+        time1 = get_length(time1)
+        time2 = get_length(time2)
+
+        difference = time1 - time2
+
+        dif_sec, dif_mins, dif_hours = convert_millis(difference)
+
+        click.echo("{0}hours {1}minutes {2}seconds".format(dif_hours, dif_mins, dif_sec))
+    except ValueError:
+        click.echo("The value that was entered is not valid")
+
+
+def convert_millis(millis):
+    seconds = (millis / 1000) % 60
+    seconds = int(seconds)
+    minutes = (millis / (1000 * 60)) % 60
+    minutes = int(minutes)
+    hours = (millis / (1000 * 60 * 60)) % 24
+    hours = int(hours)
+
+    return seconds, minutes, hours
+
+
+def get_length(millis):
+    length = len(str(millis))
+
+    if length < 10:
+        millis = str(millis)
+        millis += "000"
+
+    return float(millis)
+
+
+if __name__ == '__main__':
+    between()
